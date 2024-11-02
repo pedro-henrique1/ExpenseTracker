d14c474c5971b35504453ece4ccd33e65c599cfa
Pedro Henrique Silva Rodrigues <ph8464803@gmail.com>
2024-11-02 15:59:15 -0300

    fix: mudança para converter dado no retorno da busca de todos

--- src/main/java/com/example/expensetracker/model/Categoria.java
+++ src/main/java/com/example/expensetracker/model/Categoria.java
@@ -1,5 +1,7 @@
 package com.example.expensetracker.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 
 @Getter
@@ -12,10 +14,25 @@ public enum Categoria {
     EDUCACAO("Educação"),
     OUTROS("Outros");
 
-    private final String descricao;
+    private final String valor;
 
-    Categoria(String descricao) {
-        this.descricao = descricao;
+    Categoria(String valor) {
+        this.valor = valor;
+    }
+
+    @JsonValue
+    public String getValor() {
+        return valor;
+    }
+
+    @JsonCreator
+    public static Categoria fromValue(String value) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.valor.equalsIgnoreCase(value)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Categoria inválida: " + value);
     }
 
 }
--- src/main/java/com/example/expensetracker/model/MetodoPagamento.java
+++ src/main/java/com/example/expensetracker/model/MetodoPagamento.java
@@ -7,19 +7,35 @@ import lombok.Getter;
 
 public enum MetodoPagamento {
     DINHEIRO("Dinheiro"),
-//    CARTAO_CREDITO("Cartão de Crédito"),
-//    CARTAO_DEBITO("Cartão de Débito"),
-//    TRANSFERENCIA_BANCARIA("Transferência Bancária"),
+    CARTAO_CREDITO("Cartão de Crédito"),
+    CARTAO_DEBITO("Cartão de Débito"),
+    TRANSFERENCIA_BANCARIA("Transferência Bancária"),
     PIX("Pix"),
     BOLETO("Boleto"),
-//    APLICATIVOS_PAGAMENTO("Aplicativos de Pagamento"),
-    CHEQUE("Cheque");
-//    CARTAO_BENEFICIOS("Cartão de Benefícios");
+    APLICATIVOS_PAGAMENTO("Aplicativos de Pagamento"),
+    CHEQUE("Cheque"),
+    CARTAO_BENEFICIOS("Cartão de Benefícios");
+
 
     private final String valor;
 
     MetodoPagamento(String valor) {
         this.valor = valor;
+    }
+
+    @JsonValue
+    public String getValor() {
+        return valor;
+    }
+
+    @JsonCreator
+    public static MetodoPagamento fromValue(String value) {
+        for (MetodoPagamento metodo : MetodoPagamento.values()) {
+            if (metodo.valor.equalsIgnoreCase(value)) {
+                return metodo;
+            }
+        }
+        throw new IllegalArgumentException("Método de pagamento inválido: " + value);
     }
 
 }
