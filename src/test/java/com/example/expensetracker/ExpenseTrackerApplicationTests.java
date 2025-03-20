f0030404f9972b112d14d5f8468ba1c473ba7d17
Pedro Henrique Silva Rodrigues <ph8464803@gmail.com>
2025-03-20 01:15:27 -0300

    test: adicionado test na criação e no login do usuario

--- src/test/java/com/example/expensetracker/ExpenseTrackerApplicationTests.java
+++ src/test/java/com/example/expensetracker/ExpenseTrackerApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.expensetracker;
 
+import com.example.expensetracker.service.AuthenticationServiceTest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class ExpenseTrackerApplicationTests {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        assertNotNull(applicationContext);
     }
 
 }
