eebfeb6dafe522f9a4c910fd88b6ce1baf4c1bde
Pedro Henrique Silva Rodrigues <ph8464803@gmail.com>
2025-03-26 23:43:06 -0300

    test: ajuste com mock de repository

--- src/test/java/com/example/expensetracker/service/TestExpenseService.java
+++ src/test/java/com/example/expensetracker/service/TestExpenseService.java
@@ -1,24 +1,26 @@
 package com.example.expensetracker.service;
 
-
 import com.example.expensetracker.dtos.ExpenseDto;
 import com.example.expensetracker.dtos.RegisterUserDto;
 import com.example.expensetracker.enums.Category;
 import com.example.expensetracker.enums.PaymentMethod;
 import com.example.expensetracker.exceptions.MissingRequiredFieldException;
+import com.example.expensetracker.mapper.ExpenseMapper;
 import com.example.expensetracker.model.Expense;
 import com.example.expensetracker.model.User;
-import com.example.expensetracker.services.AuthenticationService;
+import com.example.expensetracker.repositories.ExpenseRepository;
 import com.example.expensetracker.services.ExpenseService;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.AssertionFailure;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
 import java.sql.Date;
@@ -27,139 +29,171 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
-@ActiveProfiles("test")
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 @Slf4j
 @DisplayName("Teste de validação das despesas")
 public class TestExpenseService {
 
-    @Autowired
-    private AuthenticationService userService;
+    @Mock
+    private ExpenseRepository expenseRepository;
 
-    @Autowired
+    @Mock
+    private ExpenseMapper expenseMapper;
+
+    @InjectMocks
     private ExpenseService expenseService;
 
-    private static User testUser;
+    private User testUser;
+    private User testUser2;
 
-    private static User testUser2;
+    private Expense testExpense;
 
-    @BeforeAll
-    static void setup(@Autowired AuthenticationService userService,
-                      @Autowired ExpenseService expenseService) {
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
 
-        RegisterUserDto userDto = new RegisterUserDto();
-        userDto.setEmail("testuser1@test.com");
-        userDto.setPassword("testpass");
-        testUser = userService.signup(userDto);
-        assertNotNull(testUser, "Usuário de teste não foi criado corretamente.");
+        RegisterUserDto registerUserDto = new RegisterUserDto();
+        registerUserDto.setEmail("testuser1@test.com");
+        registerUserDto.setPassword("password");
 
-        RegisterUserDto userDto2 = new RegisterUserDto();
-        userDto2.setEmail("testuser2@test.com");
-        userDto2.setPassword("testpass2");
-        testUser2 = userService.signup(userDto2);
-        assertNotNull(testUser2, "Usuário de teste não foi criado corretamente.");
+        testUser = new User();
+        testUser.setEmail(registerUserDto.getEmail());
+        testUser.setId(1);
+        testUser.setPassword(registerUserDto.getPassword());
 
+        RegisterUserDto registerUserDto2 = new RegisterUserDto();
+        registerUserDto2.setEmail("testuser2@test.com");
+        registerUserDto2.setPassword("password2");
 
-        Expense expense1 = new Expense();
-        expense1.setUser(testUser);
-        expense1.setDescription("Almoço no restaurante");
-        expense1.setPrice(new BigDecimal("35.50"));
-        expense1.setCategory(Category.ALIMENTACAO);
-        expense1.setPaymentMethod(PaymentMethod.CARTAOCREDITO);
-        expense1.setDate(Date.valueOf(LocalDate.now()));
-        expense1.setObservation("alimentação para o trabalho");
-        expenseService.saveExpense(testUser, expense1);
+        testUser2 = new User();
+        testUser2.setEmail(registerUserDto.getEmail());
+        testUser2.setId(2);
+        testUser2.setPassword(registerUserDto.getPassword());
 
-        Expense expense2 = new Expense();
-        expense2.setUser(testUser);
-        expense2.setDescription("Transporte Uber");
-        expense2.setPrice(new BigDecimal("25.00"));
-        expense2.setCategory(Category.TRANSPORTE);
-        expense2.setPaymentMethod(PaymentMethod.PIX);
-        expense2.setObservation("Transporte para o trabalho");
-        expense2.setDate(Date.valueOf(LocalDate.now()));
-        expenseService.saveExpense(testUser, expense2);
+        testExpense = new Expense();
+        testExpense.setUser(testUser);
+        testExpense.setId(1);
+        testExpense.setDescription("Almoço no restaurante");
+        testExpense.setPrice(new BigDecimal("35.50"));
+        testExpense.setCategory(Category.ALIMENTACAO);
+        testExpense.setPaymentMethod(PaymentMethod.CARTAOCREDITO);
+        testExpense.setDate(Date.valueOf(LocalDate.now()));
+        testExpense.setObservation("Alimentação para o trabalho");
+
+        ExpenseDto expenseDto = new ExpenseDto();
+        expenseDto.setId(1L);
+        expenseDto.setDescription(testExpense.getDescription());
+        expenseDto.setPrice(testExpense.getPrice());
+        expenseDto.setCategory(testExpense.getCategory());
+        expenseDto.setPaymentMethod(testExpense.getPaymentMethod());
+        expenseDto.setDate(testExpense.getDate());
+        expenseDto.setObservation(testExpense.getObservation());
+
+        lenient().when(expenseRepository.findByUser(eq(testUser))).thenReturn(List.of(testExpense));
+        lenient().when(expenseMapper.toDto(any(Expense.class))).thenAnswer(invocation -> {
+            Expense expense = invocation.getArgument(0);
+            log.info("Convertendo Expense para ExpenseDto: {}", expense);
+
+            ExpenseDto dto = new ExpenseDto();
+            dto.setId(Long.valueOf(expense.getId()));
+            dto.setDescription(expense.getDescription());
+            dto.setPrice(expense.getPrice());
+            dto.setCategory(expense.getCategory());
+            dto.setPaymentMethod(expense.getPaymentMethod());
+            dto.setDate(expense.getDate());
+            dto.setObservation(expense.getObservation());
+
+            return dto;
+        });
+        lenient().when(expenseRepository.save(any(Expense.class))).thenAnswer(invocation -> invocation.getArgument(0));
+        lenient().when(expenseRepository.findById(eq(1L))).thenReturn(Optional.of(testExpense));  // Garantir que a despesa com ID 1L seja encontrada
+
     }
 
-
     @Test
-    @DisplayName("Buscar dados de despesas do usuario")
+    @DisplayName("Buscar despesas do usuário")
     public void buscarDadosDespesas() {
-        List<?> expenses = expenseService.getAllExpenses(testUser);
-        log.info("Lista de despesas: {}", expenses);
-        log.info("Usuário de teste: {}", testUser);
-        assertNotNull(expenses, "A lista de despesas não deve ser nula.");
-        assertTrue(expenses.size() >= 2, "O usuário deve ter pelo menos duas despesas cadastradas.");
+        List<ExpenseDto> expenses = expenseService.getAllExpenses(testUser);
+        verify(expenseRepository).findByUser(testUser);
+        log.info("Despesas retornadas: {}", expenses);
+        assertNotNull(expenses);
+        assertEquals(1, expenses.size());
+        assertEquals("Almoço no restaurante", expenses.getFirst().getDescription());
     }
 
     @Test
-    @DisplayName("Buscar dados de uma despesa do usuario")
+    @DisplayName("Buscar uma despesa específica do usuário")
     public void buscarDadosDespesasUsuario() {
-        ExpenseDto expense = expenseService.getExpense(1L, testUser);
-        log.info("Despesa encontrada: {}", expense.toString());
-        assertNotNull(expense, "A despesa não deve ser nula.");
+        ExpenseDto result = expenseService.getExpense(Long.valueOf(testUser.getId()), testUser);
+        verify(expenseRepository).findById(eq(Long.valueOf(testUser.getId())));
+        log.info("Despesa retornada: {}", result);
+
+
+        assertNotNull(result);
+        assertEquals("Almoço no restaurante", result.getDescription());
     }
 
-
     @Test
-    @DisplayName("Deve lançar AccessDeniedException quando a despesa não pertencer ao usuário")
+    @DisplayName("Erro ao acessar despesa de outro usuário")
     public void erroDeAcessoDespesas() {
         AccessDeniedException exception = assertThrows(AccessDeniedException.class,
-                () -> expenseService.getExpense(1L, testUser2));
+                () -> expenseService.getExpense(Long.valueOf(testUser.getId()), testUser2));
+
         assertEquals("Acesso negado: a despesa não pertence ao usuário.", exception.getMessage());
     }
 
-
     @Test
-    @DisplayName("Deve lançar RuntimeException quando a despesa não foi encontrada")
+    @DisplayName("Erro ao buscar despesa inexistente")
     public void despesaNaoEncontrada() {
+        when(expenseRepository.findById(40L)).thenReturn(Optional.empty());
         RuntimeException exception = assertThrows(RuntimeException.class,
-                () -> expenseService.getExpense(40L, testUser2));
+                () -> expenseService.getExpense(40L, testUser));
         assertEquals("Despesa não encontrada", exception.getMessage());
     }
 
     @Test
+    @DisplayName("Criar despesa com sucesso")
     public void criarDespesaComSucesso() {
-        Expense novaDespesa = new Expense();
-        novaDespesa.setDescription("Compra de mercado");
-        novaDespesa.setPrice(new BigDecimal("150.00"));
-        novaDespesa.setCategory(Category.ALIMENTACAO);
-        novaDespesa.setPaymentMethod(PaymentMethod.PIX);
-        novaDespesa.setDate(Date.valueOf(LocalDate.now()));
-        novaDespesa.setObservation("Compras do mês");
-        Expense expense = expenseService.saveExpense(testUser, novaDespesa);
+        Expense newExpense = new Expense();
+        newExpense.setUser(testUser);
+        newExpense.setDescription("Café da manhã");
+        newExpense.setPrice(new BigDecimal("15.00"));
+        newExpense.setCategory(Category.ALIMENTACAO);
+        newExpense.setPaymentMethod(PaymentMethod.PIX);
+        newExpense.setDate(Date.valueOf(LocalDate.now()));
+        newExpense.setObservation("Café na padaria");
 
-        assertNotNull(expense, "A despesa criada não deve ser nula.");
-        assertNotNull(expense.getId(), "A despesa criada deve ter um ID.");
-        assertEquals("Compra de mercado", expense.getDescription());
+        when(expenseRepository.save(any(Expense.class)))
+                .thenAnswer(invocation -> invocation.getArgument(0));
+        Expense savedExpense = expenseService.saveExpense(testUser, newExpense);
+
+
+        assertNotNull(savedExpense);
+        assertEquals("Café da manhã", savedExpense.getDescription());
+
+        verify(expenseRepository, times(1)).save(any(Expense.class));
+
     }
 
     @Test
+    @DisplayName("Erro ao criar despesa sem usuário")
     public void criarDespesaSemUsuario() {
         Expense novaDespesa = new Expense();
         novaDespesa.setDescription("Compra de mercado");
-        novaDespesa.setPrice(new BigDecimal("150.00"));
-        novaDespesa.setCategory(Category.ALIMENTACAO);
-        novaDespesa.setPaymentMethod(PaymentMethod.PIX);
-        novaDespesa.setDate(Date.valueOf(LocalDate.now()));
-        novaDespesa.setObservation("Compras do mês");
 
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
                 () -> expenseService.saveExpense(null, novaDespesa));
 
-        assertEquals("O usuario nao pose ser invalido", exception.getMessage());
+        assertEquals("O usuario nao pode ser invalido", exception.getMessage());
     }
 
     @Test
+    @DisplayName("Erro ao criar despesa com valor negativo")
     public void criarDespesaComValorNegativo() {
         Expense novaDespesa = new Expense();
-        novaDespesa.setDescription("Compra de mercado");
         novaDespesa.setPrice(new BigDecimal("-150.00"));
-        novaDespesa.setCategory(Category.ALIMENTACAO);
-        novaDespesa.setPaymentMethod(PaymentMethod.PIX);
-        novaDespesa.setDate(Date.valueOf(LocalDate.now()));
-        novaDespesa.setObservation("Compras do mês");
 
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
                 () -> expenseService.saveExpense(testUser, novaDespesa));
@@ -168,14 +202,14 @@ public class TestExpenseService {
     }
 
     @Test
+    @DisplayName("Erro ao criar despesa com campos inválidos")
     public void criarDespesaComValorInvalido() {
         Expense novaDespesa = new Expense();
+        novaDespesa.setCategory(null);
         novaDespesa.setDescription("");
-        novaDespesa.setPrice(new BigDecimal("1.1"));
-        novaDespesa.setCategory(Category.ALIMENTACAO);
-        novaDespesa.setPaymentMethod(PaymentMethod.PIX);
-        novaDespesa.setDate(Date.valueOf(LocalDate.now()));
-        novaDespesa.setObservation("Compras do mês");
+        novaDespesa.setDate(null);
+        novaDespesa.setPaymentMethod(null);
+        novaDespesa.setPrice(new BigDecimal("150.00"));
 
         MissingRequiredFieldException exception = assertThrows(MissingRequiredFieldException.class,
                 () -> expenseService.saveExpense(testUser, novaDespesa));
@@ -184,30 +218,33 @@ public class TestExpenseService {
     }
 
     @Test
+    @DisplayName("Atualizar despesa existente")
     public void atualizarExpense() {
+        // Dado que a despesa já existe
+        testExpense.setDescription("Almoço no restaurante");
+        testExpense.setPrice(new BigDecimal("50.00"));
+        testExpense.setObservation("Almoço de negócios");
+
+        when(expenseRepository.findById(1L)).thenReturn(Optional.of(testExpense));
+
+        // Cria um DTO com as novas informações
         ExpenseDto novaDespesa = new ExpenseDto();
         novaDespesa.setDescription("Compra de mercado");
         novaDespesa.setPrice(new BigDecimal("150.00"));
-        novaDespesa.setCategory(Category.ALIMENTACAO);
-        novaDespesa.setPaymentMethod(PaymentMethod.PIX);
-        novaDespesa.setDate(Date.valueOf(LocalDate.now()));
-        novaDespesa.setObservation("Compra de mercado");
-        Expense expense = expenseService.saveExpense(testUser, Expense.fromDto(novaDespesa));
-        expense.setPrice(new BigDecimal("1500.00"));
-        expense.setObservation("Compra de mercado do mes");
+        novaDespesa.setObservation("Compras do mês");
         expenseService.updateExpense(1L, testUser, novaDespesa);
 
-        assertEquals("Compra de mercado do mes", expense.getObservation());
-        assertEquals("1500.00", expense.getPrice().toString());
-
-
+        assertEquals("Compras do mês", testExpense.getObservation());
+        assertEquals("150.00", testExpense.getPrice().toString());
+        verify(expenseRepository, times(1)).findById(1L);
     }
 
 
-
-
-
-
-
-
+    @Test
+    @DisplayName("Excluir despesa existente")
+    public void deleteExpense() {
+        doNothing().when(expenseRepository).delete(any(Expense.class));
+        expenseService.deleteExpense(testUser, Long.valueOf(testUser.getId()));
+        verify(expenseRepository, times(1)).delete(any(Expense.class));
+    }
 }
